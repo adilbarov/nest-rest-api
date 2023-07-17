c824bd0acb6cb14aa405e11a9df2071bb433c003
ArturDilbarov <112335091+xarlamovl@users.noreply.github.com>
2023-07-18 04:23:03 +0500

    Update product.service.ts

--- src/products/product.service.ts
+++ src/products/product.service.ts
@@ -3,7 +3,6 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { Product, ProductDocument } from "./schemas/product.schema";
-import { promises } from "dns";
 import { UpdateProductDto } from "./dto/update-product.dto";
 
 @Injectable()
